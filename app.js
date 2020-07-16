7a3466a5136e1c9cd72b54c7dafc12efc0b3e003
Ben Stoica <bennystoica@gmail.com>
2020-07-15 21:02:33 -0400

    styles and added sanitization

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const methodOverride = require('method-override');
-const bodyParser     = require('body-parser');
-const mongoose       = require('mongoose');
-const express        = require('express');
-const app            = express();
+const expressSanitizer = require('express-sanitizer');
+const methodOverride   = require('method-override');
+const bodyParser       = require('body-parser');
+const mongoose         = require('mongoose');
+const express          = require('express');
+const app              = express();
 
 // app config
 mongoose.connect('mongodb://localhost/blog_app', { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded( {extended: true }));
 app.use(methodOverride('_method'));
+app.use(expressSanitizer());
 
 // mongoose model config
 const blogSchema = new mongoose.Schema({
@@ -44,6 +46,7 @@ app.get('/blogs/new', function(req, res){
 
 //create route
 app.post('/blogs', function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err) {
             res.render('new');
@@ -77,6 +80,7 @@ app.get('/blogs/:id/edit', function(req, res){
 
 //update route
 app.put('/blogs/:id', function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err) {
             res.redirect('/blogs');
