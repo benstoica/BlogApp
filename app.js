aeefb574a96c591e0b3da1845757c8ad83d30d03
Ben Stoica <bennystoica@gmail.com>
2020-07-15 20:39:15 -0400

    added delete route

--- app.js
+++ app.js
@@ -86,6 +86,17 @@ app.put('/blogs/:id', function(req, res) {
     });
 });
 
+//delete route
+app.delete('/blogs/:id', function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+});
+
 
 
 app.listen(3000, process.env.IP, function(){
